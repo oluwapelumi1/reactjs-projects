9dbc6ca84d5dc6ac8c9fdd1561d3b4ba0af0786c
oluwapelumi1 <oluwapelumidaniel88@gmail.com>
2024-06-03 14:02:03 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-// import Accordian from "./components/accordian";
-import RandomColor from "./components/randon-color";
+//import Accordian from "./components/accordian";
+// import RandomColor from "./components/randon-color";
+// import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-      {/* Accordian component */}
-      {/* <Accordian/> */}
+      {/* Accordian component
+      <Accordian/> */}
 
-      {/* Random color component */}
-      <RandomColor/>
+      {/* Random color component
+      <RandomColor/> */}
+
+      {/* star reting component
+      < StarRating noOfStars={10}/> */}
     </div>
   );
 }
